f1e71945c1ed849c094ae32b991f3ada2705ecf7
devwithaditya <rajadityafeb22@gmail.com>
2026-03-29 16:57:05 +0530

    issue resolve

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/calendly-clone/", // ✅ THIS FIXES 404
+
   plugins: [
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
